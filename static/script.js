13e9186376a0c5f9b2be35035155db2c0bafbb7d
Debjyoti121 <sarkardebjyoti733@gmail.com>
2024-12-06 14:52:25 +0530

    debjyoti

--- static/script.js
+++ static/script.js
@@ -22,7 +22,7 @@ document.getElementById("imageForm").addEventListener("submit", async (e) => {
 
   try {
     // Send a POST request to the backend
-    const response = await fetch("http://127.0.0.1:5000/generate", {
+    const response = await fetch("http://127.0.0.1:10000/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
